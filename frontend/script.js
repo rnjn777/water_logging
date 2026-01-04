103d1fe29fe91411fa19127a670aa41e69cfa3ad
Vansh Tanwar <tanwarvansh1822006@gmail.com>
2026-01-04 16:33:14 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
-fetch("http://localhost:5000/api/reports")
+const API_BASE_URL = "https://water-logging.onrender.com";
+fetch(`${API_BASE_URL}/api/reports`)
+
   .then(res => res.json())
   .then(data => {
     console.log("Reports from DB:", data);
